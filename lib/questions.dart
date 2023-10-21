20f820a84d8b53f0e8f8cdb9bba12f785247a854
Abhishek Raina <abhishek.raina.uit@gmail.com>
2023-10-21 22:21:16 +0530

    List is filled with Icon widgets Added the questions.dart file to handle the question and aswers together in a single list. Added comments about solved problem with question class

---
+++ lib/questions.dart
@@ -0,0 +1,15 @@
+class Questions {
+  late String myString;
+  late bool myBoolean;
+
+  Questions(String myString, bool myBoolean) {
+    this.myString = myString;
+    this.myBoolean = myBoolean;
+  }
+}
+
+
+//We solved the Issues of null safety and we learned the follwing
+//1. Using late keyword helped in dealing with null-safety error
+//2. Using the 'this keyword' helped in accessing the late defined instances during the runtime.
+
