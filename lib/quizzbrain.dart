bc8eb9b8ae4b024e2e636bf8d75e84f0e706d7da
Abhishek Raina <abhishek.raina.uit@gmail.com>
2023-10-23 23:07:57 +0530

    List is filled with Icon widgets Added the questions.dart file to handle the question and aswers together in a single list. Added comments about solved problem with question class
    Added the quizzbrain.dart file to modularize our code base.
    
    Removed the questionreserve from questions.dart and moved it to the quizzbrain.dart file and added more questions(about 13)
    We added a function in the quizzbrain.dart which will keep track of numnber of questions and hence we solved the exception of out of bounds for the list.

--- lib/quizzbrain.dart
+++ lib/quizzbrain.dart
@@ -2,14 +2,17 @@
 // this file is used to provide the questions and answers via questions file which has the constructor to guide the actions
 import 'questions.dart';
 
-class Quizzbrain{
-  List<Questions> questionreserve =[
+class Quizzbrain {
+   int _count=0;
+  final List<Questions> _questionreserve = [
+    //I made this list a private member so that main can't have direct access to questions and answers.
 
     Questions('Some cats are actually allergic to humans', true),
     Questions('You can lead a cow down stairs but not up stairs.', false),
-    Questions('Approximately one quarter of human bones are in the feet.', true),
+    Questions(
+        'Approximately one quarter of human bones are in the feet.', true),
     Questions('A slug\'s blood is green.', true),
-    Questions('Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', true),
+    Questions('Buzz Aldrin\'s mother\'s maiden name was Moon.', true),
     Questions('It is illegal to pee in the Ocean in Portugal.', true),
     Questions(
         'No piece of square dry paper can be folded in half more than 7 times.',
@@ -30,6 +33,24 @@ class Quizzbrain{
     Questions(
         'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
         true),
+
   ];
+
+
+  // Made following methods which will only pass the answers and questions required by the main.dart file not the whole list's access.
+  // by doing so we made our code more abstract and encapsulated with high security.
+void nextQuestion() {
+  if (_count < _questionreserve.length-1) {
+   _count++;
+  }
 }
+    String getQuestionText() {
+      return _questionreserve[_count].myString;
+    }
+    bool getAnswer() {
+      return _questionreserve[_count].myBoolean;
+    }
+  }
+
+
 
